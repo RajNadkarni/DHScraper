3bf3f2cc5943adb8c2519a70479631723e5afec5
Aryan Vora <voraaryan1@gmail.com>
2024-05-22 18:17:48 -0700

    basic request

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+import requests
+from bs4 import BeautifulSoup
+url = "https://nutrition.sa.ucsc.edu/"
+
+response = requests.get(url, verify=False)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    locations = soup.find_all(class_='locations')
+    for item in locations:
+        print(item.text.strip())
+
+else:
+    print("Failed to retrieve the webpage")
