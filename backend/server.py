7c520b52bacac07a336ed32639c0eb4c5edc7775
Raj <ranadkar@ucsc.edu>
2024-05-22 18:56:01 -0700

    DAAL SAAG

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib3
+import re
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url = "https://nutrition.sa.ucsc.edu/"
 
-response = requests.get(url, verify=False)
+# Create a Session object
+s = requests.Session()
+s.verify = False  # Disable SSL verification for the session
+
+response = s.get(url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -14,12 +19,20 @@ if response.status_code == 200:
 
     links = [item.find('a').get('href') for item in locations[:3]]
     for link in links:
-        resp = requests.get(url + link, verify=False)
+        # shortmenu.aspx?sName=UC Santa Cruz Dining&locationNum=40&locationName=College Nine/John R. Lewis Dining Hall&naFlag=1
+        # Extract College Nine
+        location = re.search(r'locationName=(.+?)&', link).group(1).replace('+', ' ').replace('%2f', '/')
+
+        resp = s.get(url + link)  # Use the session to make the request
         soup = BeautifulSoup(resp.content, 'html.parser')
-        print(soup)
         meals = soup.find_all(class_='shortmenurecipes')
         for meal in meals:
-            print(meal)
+            if meal.text.strip() == 'Daal Saag':
+                print("Daal Saag is available at", location, "(:")
+                break
+        else:
+            print("Daal Saag is not available at", location, "):")
+                
 
 else:
     print("Failed to retrieve the webpage")
